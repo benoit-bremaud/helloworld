e5f22b3517f58c4bbc3bc509fb6aa0422aa02164
Benoit Bremaud <benoit.bremaud@ynov.com>
2025-09-05 15:01:39 +0200

    Add route to get user by ID

--- serveur.js
+++ serveur.js
@@ -13,5 +13,16 @@ app.get('/users', (req, res) => {
   res.send(users);
 });
 
+// Route getUserById
+app.get('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const user = users.find(u => u.id === userId);
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send({ message: 'User not found' });
+  }
+});
+
 // Démarrer le serveur
 app.listen(5000, () => console.log('🚀 Serveur lancé sur http://localhost:5000'));
